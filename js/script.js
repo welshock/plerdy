661630ddd24028cf2659a960883d8f9991c7f34e
renjii71 <taras.kozak08@gmail.com>
2024-12-02 00:25:12 +0200

    stylized page

--- js/script.js
+++ js/script.js
@@ -9,6 +9,26 @@ const swiper = new Swiper('.swiper', {
   },
   grabCursor: true,
 
-  slidesPerView: 3,
+  slidesPerView: 3.3,
   spaceBetween: 17,
+  breakpoints: {
+    1000: {
+        slidesPerView: 3.3,
+    },
+
+    768: {
+        slidesPerView: 2.3,
+    },
+    484: {
+        slidesPerView: 2,
+    },
+    280: {
+        slidesPerView: 1.2,
+    },
+    100: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+    },
+},
+
 });
